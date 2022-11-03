75504ce7795df0fd8c1117b89cc3c2f72e9e7213
shakthi <shakthikumar123456789@gmail.com>
2022-11-03 23:14:48 +0530

    Create room card component

---
+++ src/components/roomCard.jsx
@@ -0,0 +1,35 @@
+const RoomCard = ({ room }) => {
+  const {
+    photo: img,
+    beds,
+    type,
+    rating,
+    title,
+    superHost,
+  } = room || { img: "", type: "", rating: 0, title: "" };
+
+  console.log(room);
+
+  return (
+    <div className="room-card">
+      <img className="room__img" src={img} alt="" />
+
+      <div className="room__details">
+        {superHost && <span className="badge">super host</span>}
+
+        <div className="type">
+          {type === "Entire apartment" ? type + ` . ${beds}beds` : type}
+        </div>
+
+        <div className="rating">
+          <span className="material-symbols-rounded">star</span>{" "}
+          {rating.toFixed(2)}
+        </div>
+      </div>
+
+      <p className="room__title">{title}</p>
+    </div>
+  );
+};
+
+export default RoomCard;
