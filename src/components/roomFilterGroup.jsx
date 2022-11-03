05755bc25f1a53c02446ad181bf4e119bbbf0124
shakthi <shakthikumar123456789@gmail.com>
2022-11-04 04:15:22 +0530

    Create filter group

---
+++ src/components/roomFilterGroup.jsx
@@ -0,0 +1,13 @@
+const FilterGroup = () => {
+  return (
+    <div className="filter-group">
+      <div className="location">Helsinki, Finland</div>
+
+      <div className="guests">Add guests</div>
+
+      <span className="material-symbols-outlined">search</span>
+    </div>
+  );
+};
+
+export default FilterGroup;
