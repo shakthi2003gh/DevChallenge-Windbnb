b60278f8fae7b66251ddf75d8486b62de239f39c
shakthi <shakthikumar123456789@gmail.com>
2022-11-05 06:18:42 +0530

    Create guest option component

---
+++ src/components/guestOption.jsx
@@ -0,0 +1,33 @@
+import Count from "./count";
+
+const GuestOption = ({ dispatch }) => {
+  const handleCountChange = (type, count) => {
+    dispatch({ type, count });
+  };
+
+  return (
+    <div className="guests-option">
+      <div className="adults">
+        <div>
+          <div className="label">adults</div>
+          <div className="rules">Ages 13 or above</div>
+        </div>
+
+        <Count
+          min={1}
+          onCount={(count) => handleCountChange("set-adults", count)}
+        />
+      </div>
+      <div className="children">
+        <div>
+          <div className="label">children</div>
+          <div className="rules">Ages 2-12</div>
+        </div>
+
+        <Count onCount={(count) => handleCountChange("set-children", count)} />
+      </div>
+    </div>
+  );
+};
+
+export default GuestOption;
