81f91f98628bfea1e0ba7d68489e35d07db36805
shakthi <shakthikumar123456789@gmail.com>
2022-11-05 13:02:27 +0530

    Update count component

--- src/components/count.jsx
+++ src/components/count.jsx
@@ -1,10 +1,14 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 
-const Count = ({ min = 0, max = 10 }) => {
+const Count = ({ onCount, min = 0, max = 10 }) => {
   const [count, setCount] = useState(min);
 
   const handleCount = (type) =>
     setCount((prev) => (type === "+" ? prev + 1 : prev - 1));
+
+  useEffect(() => {
+    onCount(count);
+  }, [count]);
 
   return (
     <div className="count">
